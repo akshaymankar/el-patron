63a5c55f884c0959174f0f5735e31da98065bc6d
Akshay Mankar <itsakshaymankar@gmail.com>
2018-02-23 18:10:18 +0100

    Show state of the locks

--- src/Model/Lock.hs
+++ src/Model/Lock.hs
@@ -4,9 +4,14 @@ import Data.List as L
 import Data.Map as M
 import Model.Pool
 import System.Directory
+import Text.Blaze
 
-data Lock = Lock { name :: String, path :: String }
+data Lock = Lock { name :: String, path :: String, state :: LockState }
 data LockState = Claimed | Unclaimed | WaitingToRecycle | Recycling
+  deriving Show
+
+instance ToMarkup LockState where
+  toMarkup x = toMarkup $ show x
 
 getAllLocks :: FilePath -> IO (Map Pool [Lock])
 getAllLocks locksPath = do
@@ -16,18 +21,18 @@ getAllLocks locksPath = do
 
 readLocks :: FilePath -> Pool -> IO [Lock]
 readLocks locksPath pool = do
-  claimedLocks <- readLocksFromDir (locksPath ++ "/" ++ pool ++ "/claimed")
-  unclaimedLocks <- readLocksFromDir (locksPath ++ "/" ++ pool ++ "/unclaimed")
-  recyclingLocks <- readLocksFromDir (locksPath ++ "/" ++ pool ++ "-lifecycle/claimed")
-  tobeRecycledLocks <- readLocksFromDir (locksPath ++ "/" ++ pool ++ "-lifecycle/unclaimed")
+  claimedLocks <- readLocksFromDir (locksPath ++ "/" ++ pool ++ "/claimed") Claimed
+  unclaimedLocks <- readLocksFromDir (locksPath ++ "/" ++ pool ++ "/unclaimed") Unclaimed
+  recyclingLocks <- readLocksFromDir (locksPath ++ "/" ++ pool ++ "-lifecycle/claimed") Recycling
+  tobeRecycledLocks <- readLocksFromDir (locksPath ++ "/" ++ pool ++ "-lifecycle/unclaimed") WaitingToRecycle
   return $ claimedLocks ++ unclaimedLocks ++ recyclingLocks ++ tobeRecycledLocks
 
-readLocksFromDir :: FilePath -> IO [Lock]
-readLocksFromDir dir = do
+readLocksFromDir :: FilePath -> LockState -> IO [Lock]
+readLocksFromDir dir state = do
   pathExists <- doesPathExist dir
   if pathExists then do
     names <- (L.delete ".gitkeep") <$> listDirectory dir
-    return $ L.map (\name -> Lock name (dir ++ name)) names
+    return $ L.map (\name -> Lock name (dir ++ name) state) names
   else
     return []
 
