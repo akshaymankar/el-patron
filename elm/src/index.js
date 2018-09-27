80b6962f87768f22bd687baf232a57c69eb24b5e
Akshay Mankar <itsakshaymankar@gmail.com>
2018-09-27 22:53:11 +0100

    Remove logic for progressive web app
    The service worker caches all the static assets and loads them from
    memory, then updates them in the background. But this comes at the cost
    of updates not going immediately. This causes problems when contracts
    between backend and frontend is not the same. Also, for some reason
    / -> /locks -> github -> / keeps looping due to the service worker, I do
    not understand why this happens.
    
    So, for now I am disabling this feature, maybe I can revisit this later.

--- elm/src/index.js
+++ elm/src/index.js
@@ -2,9 +2,5 @@ import './main.css';
 import './masonry.css';
 import './loader.css';
 import { Main } from './Main.elm';
-import registerServiceWorker from './registerServiceWorker';
 
-//Main.embed(document.getElementById('root'));
 Main.fullscreen({});
-
-registerServiceWorker();
